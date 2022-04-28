0f614a368dd6ffe462af4c1948c37f7b132cc034
Ffauzan <f.fauzan@mail.ugm.ac.id>
2022-04-28 20:20:35 +0700

    db migrations

--- prisma/migrations/20220428100649_ewewew/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Inbox_user_id_fkey` ON `Inbox`;
-
--- AddForeignKey
-ALTER TABLE `Inbox` ADD CONSTRAINT `Inbox_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
