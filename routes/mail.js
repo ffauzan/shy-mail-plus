9832858b62385470b696b107baf4dcc049976d51
Ffauzan <f.fauzan@mail.ugm.ac.id>
2022-04-28 11:19:17 +0700

    get specific locked inbox handler

--- routes/mail.js
+++ routes/mail.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 const mailController = require('../controller/mailController')
 const authMiddleware = require('../middleware/auth')
 
-// Static
 
 // Add locked inbox
 router.post('/sec-inbox', authMiddleware.userAuth, mailController.addLockedInbox)
@@ -10,8 +9,8 @@ router.post('/sec-inbox', authMiddleware.userAuth, mailController.addLockedInbox
 // Get all locked inboxes of user
 router.get('/sec-inbox', authMiddleware.userAuth, mailController.getLockedInboxes)
 
-
-// Dynamic
+// Get a locked inbox
+router.get('/sec-inbox/:inboxId', authMiddleware.userAuth, mailController.getLockedInbox)
 
 // Get inbox or message list
 router.get('/:address', mailController.getInbox)
